227234694485966ee8996ef455609f3437cfeab2
Jérémy Vanin <jeremyvanin@proton.me>
2024-04-05 22:16:28 +0200

    Initial commit

---
+++ JukohXIII_Website/JukohXIII_Website/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+using JukohXIII_Website.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Text.Json;
+
+namespace JukohXIII_Website.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            string pathData = "Data/games.json";
+            using var jsonFile = System.IO.File.OpenRead(pathData);
+            var games = JsonSerializer.Deserialize<List<MyProjects>>(jsonFile);
+            return View(games);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ JukohXIII_Website/JukohXIII_Website/Models/MyProjects.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JukohXIII_Website.Models
+{
+    public class MyProjects
+    {
+        [Required]
+        public string? Name { get; set; }
+        [Required]
+        public string? Description { get; set; }
+        [Required]
+        public string? ImagePath { get; set; }
+       
+    }
+}
