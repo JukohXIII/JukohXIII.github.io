91263793a0675fe2e74370390f6faadb75a2c415
Jérémy Vanin <jeremyvanin@proton.me>
2024-04-07 00:37:44 +0200

    WIP

--- JukohXIII_Website/JukohXIII_Website/Controllers/HomeController.cs
+++ JukohXIII_Website/JukohXIII_Website/Controllers/HomeController.cs
@@ -7,8 +7,8 @@ namespace JukohXIII_Website.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-        public HomeController(ILogger<HomeController> logger)
+            private readonly ILogger<HomeController> _logger;
+            public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
         }
